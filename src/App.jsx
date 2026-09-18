a02ca708851c584f3f2521b03a2c440a7bb80c4e
aRonnieAlsop <alsop.arron@gmail.com>
2026-09-17 18:14:41 -0700

    'wip'

--- src/App.jsx
+++ src/App.jsx
@@ -426,7 +426,7 @@ function App() {
     ctx.fillStyle = GRAY
     ctx.font = '600 21px Arial, Helvetica, sans-serif'
     ctx.fillText(
-      'Please do not reply by text. This number is not monitored for texts.',
+'Please do not reply. This number is not monitored for text messages.',
       IMAGE_WIDTH / 2,
       2820,
     )
